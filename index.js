838cb60d1b6e3c1b77d136cc5373fab598deb598
MrBidouille <monsieurbidouille@gmail.com>
2018-05-16 18:11:01 +0200

    modif

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.client();
 
-client.on('ready', () => {
+bot.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
